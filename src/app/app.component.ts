c3666b954531ad1b4ba26764e6946494bf6565de
Alexander Vu <printscierge@gmail.com>
2024-05-19 23:10:34 -0500

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule,NavbarComponent,IntroComponent,CoursesComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+
+  title = 'hello-courses';
+  isActive = false;
+  isSubscribed = true;
+
+}
